c76412a8aa53b048e1478b13f1c7716c65fa5072
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2024-05-11 14:09:24 -0300

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -20,8 +20,8 @@ function App() {
   } 
 
   return (
-    <main className='min-h-screen flex flex-col justify-center items-center gap-5 p-5 xl:p-10'>
-      <h1 className='text-2xl xl:text-6xl font-[900] stroke-2'>Calculadora de loquis</h1>
+    <main className='min-h-screen flex flex-col justify-center items-center gap-5 p-5'>
+      <h1 className='text-2xl xl:text-6xl font-[900] stroke-2 border-2 container text-center p-5 hover:bg-slate-900 transition duration-200 rounded-xl'>Calculadora de loquis</h1>
       <section className='container flex flex-col items-center lg:flex-row border-2 p-5 xl:p-10 rounded-xl gap-10'>
         <Form title='Ingresos' />
         <Form title='Gastos' />
@@ -30,7 +30,7 @@ function App() {
         <label htmlFor="notes" className='label text-xl font-semibold'>Notas</label>
         <textarea name="notes" id="notes" className='text-xl container textarea-bordered textarea textarea-accent border-white min-h-[20vh]'></textarea>
       </section>
-      <button className='btn btn-circle btn-accent fixed bottom-[2%] xl:top-[5%] right-[2%] size-[60px]' onClick={() => audioPlayer(audioRef)}>
+      <button className='btn btn-circle btn-accent fixed bottom-[2%] xl:top-[5%] right-[1%] size-[60px]' onClick={() => audioPlayer(audioRef)}>
         {playing ? (<FaPause className='text-xl' />) : (<FaPlay />)}
       </button>
       <audio src={loki} ref={audioRef}></audio>
