ab86e0a8d002724cc5b6ba1db05b9c82b2635693
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2024-05-13 18:40:04 -0300

    add: vite-pwa-plugin

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service-worker.js', {
-      scope: './'
-    });
-  })
-}
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(
+    {
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      includeAssets: ['money.svg', 'money_small.svg'],
+      manifest: {
+        name: 'Calculoqui',
+        short_name: 'PWA',
+        description: 'App para repartir gastos con el loqui',
+        background_color: 'black',
+        theme_color: 'black',
+        icons: [
+          {
+            src: 'money.svg',
+            sizes: '144x144',
+            type: 'image/svg',
+            purpose: 'any'
+          }, 
+          {
+            src: 'money_small.svg',
+            sizes: '52x52',
+            type: 'image/svg',
+            purpose: 'any'
+          }
+        ]
+      }
+    })],
 })
