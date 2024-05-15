ef09396c001a99d745a45380f88aac9f60e4365f
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2024-05-14 21:37:28 -0300

    add easter egg jeje

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,10 @@ function App() {
   const names = useNameStore((state) => state.names)
   const setNames = useNameStore((state) => state.change)
 
+  const easterEgg = names.includes('Martin') && names.includes('Fran');
+
   useEffect(() => {
-    if(names.length === 0) {
+    if (names.length === 0) {
       alert(setNames)
     }
   }, []);
@@ -31,7 +33,7 @@ function App() {
 
   return (
     <main className='min-h-screen flex flex-col justify-center items-center gap-5 p-5'>
-      <h1 className='text-2xl xl:text-6xl font-[900] stroke-2 border-2 container text-center p-5 hover:bg-slate-900 transition duration-200 rounded-xl'>Calculadora de {names.includes('Martin') && names.includes('Fran') ? 'loquis' : 'gastos'}</h1>
+      <h1 className='text-2xl xl:text-6xl font-[900] stroke-2 border-2 container text-center p-5 hover:bg-slate-900 transition duration-200 rounded-xl'>Calculadora de {easterEgg ? 'loquis' : 'gastos'}</h1>
       <section className='container flex flex-col items-center lg:flex-row border-2 p-5 xl:p-10 rounded-xl gap-10'>
         <Form title='Ingresos' names={names} />
         <Form title='Gastos' names={names} />
@@ -40,11 +42,14 @@ function App() {
         <label htmlFor="notes" className='label text-xl font-semibold'>Notas</label>
         <textarea name="notes" id="notes" className='text-xl container textarea-bordered textarea textarea-accent border-white min-h-[20vh]'></textarea>
       </section>
-      <button type="button" className='btn btn-circle btn-accent fixed bottom-[2%] xl:top-[5%] right-[1%] size-[60px]' onClick={() => audioPlayer(audioRef)}>
-        {playing ? (<FaPause className='text-xl' />) : (<FaPlay />)}
-        <p className='sr-only'>play</p>
-      </button>
-      <audio src={loki} ref={audioRef}></audio>
+      <>
+        {easterEgg && (<button type="button" className='btn btn-circle btn-accent fixed bottom-[2%] xl:top-[5%] right-[1%] size-[60px]' onClick={() => audioPlayer(audioRef)}>
+          {playing ? (<FaPause className='text-xl' />) : (<FaPlay />)}
+          <p className='sr-only'>play</p>
+          <audio src={loki} ref={audioRef}></audio>
+        </button>)}
+      </>
+
     </main>
   )
 }
